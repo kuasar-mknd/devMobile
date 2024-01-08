5986f3857e0b8935862e4ee49da4f136f6527cd3
CarolineH <73394670+CaroH502@users.noreply.github.com>
2024-01-08 09:55:56 +0000

    Add pages

---
+++ home-garden/src/views/LocalisationPage.vue
@@ -0,0 +1,12 @@
+<template>
+    <ion-page>
+      <ion-header>
+        <ion-toolbar>
+          <ion-title>Localisation</ion-title>
+        </ion-toolbar>
+      </ion-header>
+      <ion-content :fullscreen="true">
+      </ion-content>
+    </ion-page>
+  </template>
+  
