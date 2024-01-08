70c803ccc86c215b744aa7d05a28b563a58d0e3f
CarolineH <73394670+CaroH502@users.noreply.github.com>
2024-01-08 13:14:58 +0000

    change label color on click for the menu

--- home-garden/src/views/TabsPage.vue
+++ home-garden/src/views/TabsPage.vue
@@ -4,16 +4,20 @@
       <ion-router-outlet></ion-router-outlet>
       <ion-tab-bar slot="bottom">
         <ion-tab-button class="ripple" @click="setActiveTab('tab1')" tab="tab1" href="/tabs/home">
-          <ion-icon :class="{active: activeTab === 'tab1'}" aria-hidden="true" :icon="home" />
+          <ion-icon  :class="{active: activeTab === 'tab1'}" aria-hidden="true" :icon="home"/>
+          <ion-label :class="{active: activeTab === 'tab1'}" >Home</ion-label>
         </ion-tab-button>
         <ion-tab-button class="ripple" @click="setActiveTab('tab2')" tab="tab2" href="/tabs/jardin">
           <ion-icon :class="{active: activeTab === 'tab2'}" aria-hidden="true" :icon="leaf" />
+          <ion-label>Jardin</ion-label>
         </ion-tab-button>
         <ion-tab-button  class="ripple" @click="setActiveTab('tab3')" tab="tab3" href="/tabs/map">
           <ion-icon :class="{active: activeTab === 'tab3'}" aria-hidden="true" :icon="location" />
+          <ion-label>Localisation</ion-label>
         </ion-tab-button>
         <ion-tab-button  class="ripple" @click="setActiveTab('tab4')" tab="tab4" href="/tabs/user">
           <ion-icon :class="{active: activeTab === 'tab4'}" aria-hidden="true" :icon="person" />
+          <ion-label>Profile</ion-label>
         </ion-tab-button>
       </ion-tab-bar>
     </ion-tabs>
@@ -22,7 +26,7 @@
 
 <script setup lang="ts">
 import { ref, onMounted } from 'vue';
-import { IonTabBar, IonTabButton, IonTabs, IonIcon, IonPage, IonRouterOutlet } from '@ionic/vue';
+import { IonTabBar,IonLabel, IonTabButton, IonTabs, IonIcon, IonPage, IonRouterOutlet } from '@ionic/vue';
 import { location, person, home, leaf } from 'ionicons/icons';
 
 const activeTab = ref(localStorage.getItem('activeTab') || 'tab1');
@@ -44,13 +48,16 @@ onMounted(() => {
   --ripple-color: #37AA9F !important;
 }
 
-ion-tab-button:hover ion-icon {
+ion-tab-button:hover ion-icon,
+ion-tab-button:hover ion-label {
     color: #37AA9F !important;  
   }
 
-  ion-icon.active {
+ ion-icon.active {
   color: #37AA9F !important; /* Your desired color for active tabs */
 }
   
-
+ion-tab-button .active + ion-label {
+  color: #37AA9F !important;
+}
 </style>
