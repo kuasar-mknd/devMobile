58306b41a8687ef3f456a54654f29a91015c6b7d
CarolineH <73394670+CaroH502@users.noreply.github.com>
2024-01-08 14:30:03 +0000

    change name of component

--- home-garden/src/components/ButtonCTASecondary.vue
+++ home-garden/src/components/ButtonCTASecondary.vue
@@ -10,7 +10,7 @@ import { IonButton } from '@ionic/vue';
 import { defineComponent } from 'vue';
 
 export default defineComponent({
-  name: 'ButtonCTAPrimary',
+  name: 'ButtonCTASecondary',
   components: { IonButton },
   props: {
     buttonText: {
