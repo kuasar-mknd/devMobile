aca5a083377dd8059c8db12ad960e0563252bf02
Lmntrix31 <92026387+Lmntrix31@users.noreply.github.com>
2024-01-08 14:35:50 +0000

    component input text et input number

--- home-garden/src/components/InputText.vue
+++ home-garden/src/components/InputText.vue
@@ -1,11 +1,34 @@
 <template>
-    <div></div>
-  </template>
-    
-  <script>
-  </script>
-    
-    <style>
-    /* Style personnalisé pour la carte, si nécessaire */
-    </style>
-    
+  <div>
+    <label for="myInput">Nom</label>
+    <div>
+      <input
+        type="text"
+        id="myInput"
+        style="
+          width: 216px;
+          height: 44px;
+          background-color: #f0f5f2;
+          border: 1px solid #000;
+          border-radius: 5px;
+        "
+      />
+    </div>
+  </div>
+</template>
+
+<script lang="ts">
+  import { defineComponent } from 'vue';
+
+  export default defineComponent({
+    name: 'InputText',
+  });
+
+</script>
+
+<style scoped>
+  label {
+  display: block;
+  margin-bottom: 5px;
+}
+</style>
