0b8eef51def053844b16b1900186805653ade29e
Lmntrix31 <92026387+Lmntrix31@users.noreply.github.com>
2024-01-10 21:39:39 +0000

    component ButtonBack

--- home-garden/src/components/ButtonBack.vue
+++ home-garden/src/components/ButtonBack.vue
@@ -1,11 +1,34 @@
 <template>
-    <div></div>
-  </template>
-    
-  <script>
-  </script>
-    
-    <style>
-    /* Style personnalisé pour la carte, si nécessaire */
-    </style>
-    
+  <ion-chip>
+    <ion-icon :icon="chevronBack"></ion-icon>
+  </ion-chip>
+</template>
+
+<script lang="ts">
+  import { IonChip,  IonIcon } from '@ionic/vue';
+  import {  chevronBack } from 'ionicons/icons';
+  import { defineComponent } from 'vue';
+
+  export default defineComponent({
+    components: { IonChip, IonIcon },
+    setup() {
+      return {
+
+        chevronBack,
+      };
+    },
+  });
+</script>
+
+<style scoped>
+  ion-icon {
+    color: #37AA9F !important;
+  }
+
+  ion-icon {
+    font-size: 2rem;
+  }
+  ion-icon {
+    stroke-width: 3px !important;
+  }
+</style>
