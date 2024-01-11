d87364116d9c82d133dd130c24c57612b388a2e5
Lmntrix31 <92026387+Lmntrix31@users.noreply.github.com>
2024-01-11 21:55:21 +0000

    component MeteoComponent

--- home-garden/src/components/MeteoComponent.vue
+++ home-garden/src/components/MeteoComponent.vue
@@ -1,11 +1,23 @@
 <template>
-    <div></div>
-  </template>
-    
-  <script>
-  </script>
-    
-    <style>
-    /* Style personnalisé pour la carte, si nécessaire */
-    </style>
-    
+
+    <ion-img src="../../resources/IconMeteo.png"></ion-img>
+  
+</template>
+
+<script lang="ts">
+  import { IonImg } from '@ionic/vue';
+  import { defineComponent } from 'vue';
+
+  export default defineComponent({
+    name: 'MeteoComponent',
+    components: { IonImg},
+  });
+</script>
+
+<style scoped>
+  ion-img {
+    width: 7.5rem;
+    height: 6.5rem;
+    border-radius: 0.3rem;
+  }
+</style>
