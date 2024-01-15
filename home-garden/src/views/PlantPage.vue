c50c8f8d79f12503f7a8a7d6374ba9dd1332a564
Lmntrix31 <92026387+Lmntrix31@users.noreply.github.com>
2024-01-15 09:59:39 +0000

    ajout page plante

---
+++ home-garden/src/views/PlantPage.vue
@@ -0,0 +1,51 @@
+<template>
+    <ion-page>
+      <ion-content>
+        <ion-toolbar>
+          <div class="logo-container">
+            <ion-img class="logo" src="../../resources/icon.png"></ion-img>
+          </div>
+        </ion-toolbar>
+  
+          <div class="ajouter-plant-text">
+            <h1>Ajouter une plante</h1>
+          </div>
+          <ButtonPictureVue class="button-picture" />
+
+      </ion-content>
+    </ion-page>
+  </template>
+  
+  <script setup lang="ts">
+  import ButtonPictureVue from '@/components/ButtonPicture.vue';
+  import { IonPage, IonToolbar, IonContent } from '@ionic/vue';
+  </script>
+  
+  <style>
+  /* Style personnalisé pour la carte, si nécessaire */
+  
+  .logo {
+    max-width: 200px; /* Ajustez la taille de votre logo */
+    height: auto;
+    margin-right: auto;
+    margin-left: auto;
+  }
+  
+ 
+  
+  .ajouter-plant-text {
+    text-align: left;
+    color: #37AA9F;
+    font-size: 24px;
+    font-weight: bold;
+    margin-left: 2rem;
+    font-family: 'otomanopee_one' !important;
+
+  }
+  
+  .button-picture {
+    margin-left: 10%; /* Place le bouton à gauche */
+
+  }
+  </style>
+  
