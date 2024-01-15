501d67f08746f21544b007e47c8c004d2da233ea
Lmntrix31 <92026387+Lmntrix31@users.noreply.github.com>
2024-01-15 13:46:06 +0000

    creation input Url plant

---
+++ home-garden/src/components/InputUrl.vue
@@ -0,0 +1,25 @@
+<!-- InputText.vue -->
+<template>
+    <ion-input
+      :label="internalLabelText"
+      label-placement="floating"
+      fill="outline"
+      :placeholder="placeholderText"
+      type="url" 
+      :style="{ width: '216px', height: '40px', borderRadius: '5px' }"
+      class="custom-input"
+    ></ion-input>
+  </template>
+  
+  <script setup lang="ts">
+  import { ref, defineProps } from 'vue';
+  
+  const props = defineProps(['labelText']);
+  const placeholderText = ref("Entrez l'URL"); // Modifiez le texte du placeholder ici
+  const internalLabelText = props.labelText || "Nom"; // Utilisez la prop ou une valeur par défaut
+  </script>
+  
+  <style scoped>
+  /* Style personnalisé pour la carte, si nécessaire */
+  </style>
+  
