6ef8c56a5f126655c5930ae25b5322b6db826eee
Lmntrix31 <92026387+Lmntrix31@users.noreply.github.com>
2024-01-25 11:13:37 +0000

    Refactor modal content in AreaInfoPlant.vue

--- home-garden/src/components/AreaInfoPlant.vue
+++ home-garden/src/components/AreaInfoPlant.vue
@@ -5,12 +5,11 @@
     
     <!-- Modal -->
     <ion-modal :is-open="showModal" @dismiss="showModal = false">
-      <ion-content>
         <!-- Ajoutez une div pour le voile flou -->
         <div class="blur-background"></div>
 
         <!-- Contenu de votre modal -->
-        <ion-card class="modal-card">
+        <ion-card class="modal-card" >
           <ion-card-header>
             <ion-card-title>Informations</ion-card-title>
             <!-- Ajoutez un bouton de fermeture -->
@@ -29,7 +28,6 @@
             <DetailPlantBloom bloomSeason="saison de récolte exemple : Été" />
           </ion-card-content>
         </ion-card>
-      </ion-content>
     </ion-modal>
   </div>
 </template>
