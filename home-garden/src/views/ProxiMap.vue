c106f911b13bbd2c91b868b08d28eac7d34d04a7
Samuel <60432398+kuasar-mknd@users.noreply.github.com>
2024-01-27 10:56:28 +0100

    Ajouter des styles pour le logo et le bouton fixe

--- home-garden/src/views/ProxiMap.vue
+++ home-garden/src/views/ProxiMap.vue
@@ -71,3 +71,33 @@ export default {
   },
 };
 </script>
+<style scoped>
+.class-logo {
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  margin-top: 10px;
+}
+
+ion-col {
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  text-align: center;
+}
+.titre {
+  margin-left: 25px;
+}
+
+.titrePage {
+  margin-top: 15%;
+  color: #37aa9f;
+}
+
+.button-fixed {
+  position: fixed; /* Fixe la position du bouton */
+  bottom: 20px; /* 20 pixels depuis le bas de la page */
+  /* 20 pixels depuis la droite de la page */
+  z-index: 1000; /* S'assure que le bouton soit au-dessus des autres éléments */
+}
+</style>
